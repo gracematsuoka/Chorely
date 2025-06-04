f2796b8bb665f49d38b2a4ee035208b609caf18a
Grace Matsuoka <gracelmatsuoka@gmail.com>
2025-06-04 13:39:31 -0700

    Initial Commit

---
+++ Chorely/ChorelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChorelyApp.swift
+//  Chorely
+//
+//  Created by Grace Matsuoka on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ChorelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
